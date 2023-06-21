cf58ea455dabd4f782def7b036046108cc194fe5
Jose Rocha <jsantosrocha@gmail.com>
2023-06-20 21:08:49 -0300

    feat(front): add service courses

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,7 @@
+import { CoursesService } from './../services/courses.service';
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -8,12 +10,10 @@ import { Course } from '../model/course';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-  courses: Course[] = [
-    {_id: '1', name: 'Angular', category: 'front-end'}
-  ];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(){
-
+  constructor(private coursesService: CoursesService){
+      this.courses = this.coursesService.list();
   }
 }
