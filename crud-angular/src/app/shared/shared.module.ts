441196b4cf2fb3afabb17b5d8abba0806648bf50
Jose Rocha <jsantosrocha@gmail.com>
2023-06-23 21:51:08 -0300

    feat(front): add icons in category

--- crud-angular/src/app/shared/shared.module.ts
+++ crud-angular/src/app/shared/shared.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErroDialogComponent } from './components/erro-dialog/erro-dialog.component';
 import { AppMaterialModule } from './app-material/app-material.module';
+import { CategoryPipe } from './pipes/category.pipe';
 
 
 @NgModule({
   declarations: [
-    ErroDialogComponent
+    ErroDialogComponent,
+    CategoryPipe
   ],
   imports: [
     CommonModule,
     AppMaterialModule
   ],
-  exports: [ErroDialogComponent]
+  exports: [ErroDialogComponent, CategoryPipe]
 })
 export class SharedModule { }
