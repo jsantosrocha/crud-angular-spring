e747e473b781d616365dcb6de194cec97d75941d
Jose Rocha <jsantosrocha@gmail.com>
2023-08-26 23:47:32 -0300

    chore(back): replace db in memory (H2) for mariadb

--- crud-spring/src/main/java/com/zecadev/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/zecadev/crudspring/CrudSpringApplication.java
@@ -1,12 +1,12 @@
 package com.zecadev.crudspring;
 
-import org.springframework.boot.CommandLineRunner;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.context.annotation.Bean;
+// import com.zecadev.crudspring.model.Course;
+// import com.zecadev.crudspring.repository.CourseRepository;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.zecadev.crudspring.model.Course;
-import com.zecadev.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -15,21 +15,21 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner initDatabase(CourseRepository courseRepository){
-		return args -> {
-			courseRepository.deleteAll();
+	// @Bean
+	// CommandLineRunner initDatabase(CourseRepository courseRepository){
+	// 	return args -> {
+	// 		courseRepository.deleteAll();
 
-/* 			Course c1 = new Course();
-			c1.setName("Angular");
-			c1.setCategory("front-end");
-			courseRepository.save(c1);
+ 	// 		Course c1 = new Course();
+	// 		c1.setName("Angular");
+	// 		c1.setCategory("front-end");
+	// 		courseRepository.save(c1);
 
-			Course c2 = new Course();
-			c2.setName("Java");
-			c2.setCategory("back-end");
-			courseRepository.save(c2);
- */
-		};
-	}
+	// 		Course c2 = new Course();
+	// 		c2.setName("Java");
+	// 		c2.setCategory("back-end");
+	// 		courseRepository.save(c2);
+
+	// 	};
+	// }
 }
